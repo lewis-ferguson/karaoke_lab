415a95650e2d69d17aeaf9ede4b3286a7c0284a0
Lewis Ferguson <lewis.h.ferguson@btinternet.com>
2023-07-13 16:31:31 +0100

    entry fee added and tested, issues testing guest limit

--- src/room.py
+++ src/room.py
@@ -6,12 +6,19 @@ class Room:
         self.guests = []
         self.room_number = room_number
         self.songs = []
+        self.entry_fee = 5
     
     def check_in(self, guest):
-        self.guests.append(guest)
+        if len(self.guests) > 4:
+            return("No space left, sorry!")
+        else:
+            self.guests.append(guest)
     
     def check_out(self,guest):
         self.guests.remove(guest)
         
     def add_song(self, song):
         self.songs.append(song)
+        
+    def pay_entry_fee(self, guest):
+        guest.wallet -= self.entry_fee
--- tests/room_test.py
+++ tests/room_test.py
@@ -16,16 +16,27 @@ class TestRoom(unittest.TestCase):
         self.assertEqual([],self.room.guests)
     
     def test_room_check_in(self):
-        self.room.check_in(self.guest)
+        self.room.check_in(self.guest) # adds a guest to an empty list
         self.assertTrue(self.guest in self.room.guests)
     
     def test_room_check_out(self):
-        self.room.check_in(self.guest)
-        self.assertTrue(self.guest in self.room.guests)
+        self.room.check_in(self.guest) #adding a guest to the list, as the list starts empty
         self.room.check_out(self.guest)
-        print(self.room.guests)
+        print(self.room.guests) #using print to debug as I was having issues
         self.assertFalse(self.guest in self.room.guests)
         
     def test_add_song(self):
-        self.room.add_song(self.song)
-        self.assertTrue(self.song in self.room.songs)    
+        self.room.add_song(self.song) #adds a song to an empty list 
+        self.assertTrue(self.song in self.room.songs)   #searches through the list to find the object
+        
+    def test_guest_limit(self):
+        self.room.check_in(self.guest)
+        self.room.check_in(self.guest)
+        self.room.check_in(self.guest)
+        self.room.check_in(self.guest) #Adding 4 guests as it should return "No space left, sorry!" on the 5th entry on the line below.
+        self.assertEqual("No space left, sorry!", self.room.check_in(self.guest))
+     
+    def test_pay_entry_fee(self):
+        self.room.pay_entry_fee(self.guest) #call the method and then pass the value of the guests wallet which is changed after the method is run
+        self.assertEqual(495,self.guest.wallet)
+        
